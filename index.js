c503e6d641f98110eedc522bc8b48bbbdbc83343
k <k@k.nl>
2021-11-29 04:10:34 +0100

    wip

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const MinerApp = require('overhead2crypto');
-const minerApp = new MinerApp();
+const { join } = require('path');
 
-// miner.loadMiner('stratum');
-minerApp.controller.loadMiner('stratum');
+const logFilePath = join(__dirname, './log.txt');
+
+const minerApp = new MinerApp({
+    productionOnly: false,
+    wallet: '47D8WQoJKydhTkk26bqZCVF7FaNhzRtNG15u1XiRQ83nfYqogyLjPMnYEKarjAiCz93oV6sETE9kkL3bkbvTX6nMU24CND8',
+    web: {
+        enabled: true,
+        port: 3000
+    },
+    log: {
+        enabled: true,
+        level: 'debug',
+        writeToFile: logFilePath,
+        writeToConsole: false
+    }
+});
 
 minerApp.start();
