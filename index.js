66628192142f0ff7903fc17b85da42dcae42e25b
k <k@k.nl>
2021-12-01 17:09:25 +0100

    wip

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const MinerApp = require('overhead2crypto');
+const MinerApp = require('eazyminer');
 const { join } = require('path');
 
 const logFilePath = join(__dirname, './log.txt');
@@ -6,15 +6,14 @@ const logFilePath = join(__dirname, './log.txt');
 const minerApp = new MinerApp({
     productionOnly: false,
     wallet: '47D8WQoJKydhTkk26bqZCVF7FaNhzRtNG15u1XiRQ83nfYqogyLjPMnYEKarjAiCz93oV6sETE9kkL3bkbvTX6nMU24CND8',
+    url: 'xmrpool.eu:9999',
     web: {
         enabled: true,
         port: 3000
     },
     log: {
-        enabled: true,
-        level: 'debug',
         writeToFile: logFilePath,
-        writeToConsole: false
+        writeToConsole: true
     }
 });
 
